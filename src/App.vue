596f9b761995cca614a10b4fe5dffa087d79b0a3
Dexterwe <51189959+Dexterwe@users.noreply.github.com>
2020-08-11 20:23:55 +0800

    博客文件
    src为前端 sever为后端

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="root">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name : "App"
+  }
+</script>
+
+<style lang="less">
+  @import "assets/css/reset.css";
+  @import "assets/css/font.css";
+  #root{
+    width: 100%;
+    /*height: 3000px;*/
+    background: url("./assets/img/bg.jpg") center top/cover fixed;
+  }
+</style>
